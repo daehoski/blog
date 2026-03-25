8607bc9b4efaa5855496c34604970001641bbc25
daeho <daeho@test.pri>
2026-03-25 19:39:53 +0900

    style(header): Shorten site title to AWS SAA and optimize mobile navigation space

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'AWS SAA C03 실전 대비 가이드';
+export const SITE_TITLE = 'AWS SAA';
 export const SITE_DESCRIPTION = '725개의 AWS SAA-C03 실전 연습 문제와 상세 해설을 제공하는 비영리 인터랙티브 학습 포털입니다.';
